0f8a4ba95afd02063f9dc48ac6363655e27bf5a6
Jeroomvdb <125352201+Jeroomvdb@users.noreply.github.com>
2023-08-04 17:20:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+
+// IMPORT ICONS FONTAWESOME
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCat } from '@fortawesome/free-solid-svg-icons';
+import { faDog } from '@fortawesome/free-solid-svg-icons';
+import { faFrog } from '@fortawesome/free-solid-svg-icons';
+import { faDragon } from '@fortawesome/free-solid-svg-icons';
+import { faFishFins } from '@fortawesome/free-solid-svg-icons';
+
+// VARIABLES
+
+const data ={
+  items: [
+    {
+      name: "cat",
+      icon: faCat,
+      sound: "/Sounds/Cat.wav"
+    },
+    {
+      name: "dog",
+      icon: faDog,
+      sound: "/Sounds/Dog.wav"
+    },
+    {
+      name: "frog",
+      icon: faFrog,
+      sound: "/Sounds/Frog.wav"
+    },
+    {
+      name: "dragon",
+      icon: faDragon,
+      sound: "/Sounds/Dragon.wav"
+    },
+    {
+      name: "fish",
+      icon: faFishFins,
+      sound: "/Sounds/Fish.wav"
+    },
+  ]
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="animalCollection">
+        {data.items.map((item) => 
+          {console.log(item.sound);
+          console.log(new Audio(item.sound))
+          return <FontAwesomeIcon className="Animal" icon={item.icon} key={item.name} onClick={() => new Audio(item.sound).play()} >{item.icon}</FontAwesomeIcon>
+        })}
+      </div>
     </div>
   );
 }
