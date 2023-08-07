de14a6fe9c97c80344dba359ffd877234d68a733
Jeroomvdb <125352201+Jeroomvdb@users.noreply.github.com>
2023-08-07 11:23:20 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,57 +1,103 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react"; // Add this import statement
 
 // IMPORT ICONS FONTAWESOME
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCat } from '@fortawesome/free-solid-svg-icons';
-import { faDog } from '@fortawesome/free-solid-svg-icons';
-import { faFrog } from '@fortawesome/free-solid-svg-icons';
-import { faDragon } from '@fortawesome/free-solid-svg-icons';
-import { faFishFins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { faFrog } from "@fortawesome/free-solid-svg-icons";
+import { faDragon } from "@fortawesome/free-solid-svg-icons";
+import { faFishFins } from "@fortawesome/free-solid-svg-icons";
 
 // VARIABLES
 
-const data ={
+const data = {
   items: [
     {
       name: "cat",
       icon: faCat,
-      sound: "/Sounds/Cat.wav"
+      sound: "/Sounds/Cat.wav",
+      quote: "What does the cat say?",
     },
     {
       name: "dog",
       icon: faDog,
-      sound: "/Sounds/Dog.wav"
+      sound: "/Sounds/Dog.wav",
+      quote: "What does the dog say?",
     },
     {
       name: "frog",
       icon: faFrog,
-      sound: "/Sounds/Frog.wav"
+      sound: "/Sounds/Frog.wav",
+      quote: "What does the frog say?",
     },
     {
       name: "dragon",
       icon: faDragon,
-      sound: "/Sounds/Dragon.wav"
+      sound: "/Sounds/Dragon.wav",
+      quote: "What does the dragon say?",
     },
     {
       name: "fish",
       icon: faFishFins,
-      sound: "/Sounds/Fish.wav"
+      sound: "/Sounds/Fish.wav",
+      quote: "What does the fish say?",
     },
-  ]
+  ],
+};
+
+function Soundboard({ items }) {
+  const [quote, setQuote] = useState(null);
+  return (
+    <>
+      {quote && <div className="quote">{quote}</div>}{" "}
+      <div className="soundboard">
+        {items.map((item) => (
+          <SoundboardItem
+            item={item}
+            key={item.name}
+            onSoundStart={(quote) => setQuote(quote)}
+            onSoundStop={() => setQuote(null)}
+          />
+        ))}{" "}
+      </div>
+    </>
+  );
 }
 
+function SoundboardItem({ item, onSoundStart, onSoundStop }) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audio = new Audio(item.sound);
 
+  audio.addEventListener("ended", () => {
+    setIsPlaying(false);
+    onSoundStop();
+  });
+
+  function playSound() {
+    if (!isPlaying) {
+      audio.play();
+      setIsPlaying(true);
+      onSoundStart(item.quote);
+    }
+  }
+
+  return (
+    <div
+      /*className={`Animal ${isPlaying ? "playing" : ""}`}*/ onClick={playSound}
+    >
+      <FontAwesomeIcon
+        icon={item.icon}
+        className={`Animal ${isPlaying ? "playing" : ""}`}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <div className="animalCollection">
-        {data.items.map((item) => 
-          {console.log(item.sound);
-          console.log(new Audio(item.sound))
-          return <FontAwesomeIcon className="Animal" icon={item.icon} key={item.name} onClick={() => new Audio(item.sound).play()} >{item.icon}</FontAwesomeIcon>
-        })}
-      </div>
+      <Soundboard items={data.items} />
     </div>
   );
 }
